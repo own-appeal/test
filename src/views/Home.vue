c1f1405e621ce7e74c92002c488fa35dd8393754
Thembani Fubesi <tfubesi@gmail.com>
2020-02-03 22:30:09 +0200

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ion-page">
-    <h1>Converter</h1>
+    <h1>Teting 123</h1>
     <ion-content class="ion-padding">
       <h1>
         <ion-text color="dark">#1 leading stress converter</ion-text>
